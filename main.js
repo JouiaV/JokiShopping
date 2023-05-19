0efc2cf151d2d8bb75cad2577a802156a2e97414
JouiaV <127497066+JouiaV@users.noreply.github.com>
2023-05-19 14:27:35 +0300

    Color-picker

--- main.js
+++ main.js
@@ -9,32 +9,55 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppinglistCheckedInDB = ref(database, "shoppingList/checked")
-const shoppinglistUncheckedInDB = ref(database, "shoppingList/unchecked")
 
+// Locations in DB
+const shoppinglistCheckedInDB_PATH = "shoppingList/checked"  // "shoppingList-DEV/checked"
+const shoppinglistCheckedInDB = ref(database, shoppinglistCheckedInDB_PATH)
+const shoppinglistUncheckedInDB_PATH = "shoppingList/unchecked"  // "shoppingList-DEV/checked"
+const shoppinglistUncheckedInDB = ref(database, shoppinglistUncheckedInDB_PATH)
+
+// const elems
 const inputFieldElem = document.querySelector("#input-field")
 const addButtonElem = document.querySelector("#add-button")
 const shoppingListUncheckedElem = document.querySelector("#shopping-list-unchecked")
 const shoppingListCheckedElem = document.querySelector("#shopping-list-checked")
 const dropDownListElem = document.querySelector("#drop-down-list")
 
+// color selector
+const redRdElem = document.querySelector("#red")
+const orangeRdElem = document.querySelector("#orange")
+const yellowRdElem = document.querySelector("#yellow")
+const greenRdElem = document.querySelector("#green")
+const blueRdElem = document.querySelector("#blue")
+
 const shoppingItemAnimationTime = 150  // milliseconds
 
 addButtonElem.addEventListener("click", add_clicked)
 
+// Add item triggered
 function add_clicked() {
     let inputValue = inputFieldElem.value
-    addButtonElem.classList += " active" 
+    addButtonElem.classList.add("active")
+    
+    // color pick
+    let colorValue;
+    if (redRdElem.checked) {colorValue = "red"}
+    else if (orangeRdElem.checked) {colorValue = "orange"}
+    else if (yellowRdElem.checked) {colorValue = "yellow"}
+    else if (greenRdElem.checked) {colorValue = "green"}
+    else if (blueRdElem.checked) {colorValue = "blue"}
+
+    inputFieldElem.value = "" 
+
     window.setTimeout(() => {
-        addButtonElem.classList -= " active"
+        addButtonElem.classList.remove("active")
         if (isInputValid(inputValue)) {
-            push(shoppinglistUncheckedInDB, inputValue)
-            inputFieldElem.value = ""
+            push(shoppinglistUncheckedInDB, {"value": inputValue, "color": colorValue}) 
         }
     }, 125)
-    
 }
 
+// validation check
 function isInputValid(input) {
     if (input === "") {
         return false
@@ -47,17 +70,15 @@ function isInputValid(input) {
     return false
 }
 
-// UNCHECKED + + + + + + + + + + + + + + + + + +
+// UNCHECKED value changed listener + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ++ + + + + + + + + + + + + + + + + +
 onValue(shoppinglistUncheckedInDB, function(snapshot) {
     if (!snapshot.exists()) {
         console.log("Ei ole mitään unchecked")
         shoppingListUncheckedElem.innerHTML = "<a class='ei_ole_unchecked'>Lista on tyhjä...</a>"
         return
     }
-
     let itemsArray = Object.entries(snapshot.val())
     itemsArray.reverse()
-
     console.log("unchecked:")
     console.log(itemsArray)
     shoppingListUncheckedElem.innerHTML = ""
@@ -69,7 +90,7 @@ onValue(shoppinglistUncheckedInDB, function(snapshot) {
 })
 
 
-// CHECKED - - - - - - - - - - - - - - - - - - - - - 
+// CHECKED value changed listener - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 onValue(shoppinglistCheckedInDB, function(snapshot) {
     if (!snapshot.exists()) {
         console.log("Ei ole mitään checked")
@@ -92,14 +113,18 @@ onValue(shoppinglistCheckedInDB, function(snapshot) {
 
 
 // ========================================================================================================
-// UNCHECKED + + + + + + + + + + + + + + + + + +
+// create new UNCHECKED item + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
 function addNewItemToUncheckedShoppingList(item) {
+
     let itemID = item[0]
-    let itemValue = item[1]
+    let itemValue = item[1]["value"]
+    let itemColor = item[1]["color"]
 
     // Main li elem
     let liElem = document.createElement("li")
+    liElem.classList.add(`color-${itemColor}`)
     liElem.id = itemID
+    liElem.setAttribute = ("color", itemColor)
 
     // List item value
     let textElem = document.createElement("a")
@@ -109,6 +134,7 @@ function addNewItemToUncheckedShoppingList(item) {
     let checkMarkElem = document.createElement("span")
     checkMarkElem.textContent = "✓"  // ✓, ✔
     checkMarkElem.classList.add("check_mark")
+    checkMarkElem.classList.add(`color-${itemColor}`)
 
     // Dropdown trigger
     let dropdownElem = document.createElement("div")
@@ -117,18 +143,91 @@ function addNewItemToUncheckedShoppingList(item) {
     // Dropdown content
     let dropdownContentElem = document.createElement("div")
     dropdownContentElem.classList.add("dropdown-content")
+    dropdownContentElem.classList.add(itemID)
     dropdownContentElem.classList.add("hidden")
     
-    // test
-    let testPElem = document.createElement("p")
-    testPElem.classList.add("muokkaa-p")
-    testPElem.textContent = "Muokkaa: " + itemValue
+    // EDIT BOX CONTENT < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < <
+    // Muokkaa text
+    let muokaaTextElem = document.createElement("p")
+    muokaaTextElem.classList.add("muokkaa-p")
+    muokaaTextElem.textContent = "Muokkaa: " + itemValue
+
+    // Dropdown colorpicker  
+    let colorPickerDivElem = document.createElement("div")
+    colorPickerDivElem.classList.add("color-picker")
+
+    // red
+    let redLabelElem = document.createElement("label")
+    redLabelElem.classList.add("rd")
+    let redRadioElem = document.createElement("input")
+    redRadioElem.type = "radio"
+    redRadioElem.name = "dp_color-picker"
+    redRadioElem.id = "dp_red"
+    let redCircleElem = document.createElement("div")
+    redCircleElem.classList.add("red")
+    redLabelElem.appendChild(redRadioElem)
+    redLabelElem.appendChild(redCircleElem)
+
+    // orange
+    let orangeLabelElem = document.createElement("label")
+    orangeLabelElem.classList.add("rd")
+    let orangeRadioElem = document.createElement("input")
+    orangeRadioElem.type = "radio"
+    orangeRadioElem.name = "dp_color-picker"
+    orangeRadioElem.id = "dp_orange"
+    let orangeCircleElem = document.createElement("div")
+    orangeCircleElem.classList.add("orange")
+    orangeLabelElem.appendChild(orangeRadioElem)
+    orangeLabelElem.appendChild(orangeCircleElem)
+
+    // yellow
+    let yellowLabelElem = document.createElement("label")
+    yellowLabelElem.classList.add("rd")
+    let yellowRadioElem = document.createElement("input")
+    yellowRadioElem.type = "radio"
+    yellowRadioElem.name = "dp_color-picker"
+    yellowRadioElem.id = "dp_yellow"
+    let yellowCircleElem = document.createElement("div")
+    yellowCircleElem.classList.add("yellow")
+    yellowLabelElem.appendChild(yellowRadioElem)
+    yellowLabelElem.appendChild(yellowCircleElem)
+
+    // green
+    let greenLabelElem = document.createElement("label")
+    greenLabelElem.classList.add("rd")
+    let greenRadioElem = document.createElement("input")
+    greenRadioElem.type = "radio"
+    greenRadioElem.name = "dp_color-picker"
+    greenRadioElem.id = "dp_green"
+    let greenCircleElem = document.createElement("div")
+    greenCircleElem.classList.add("green")
+    greenLabelElem.appendChild(greenRadioElem)
+    greenLabelElem.appendChild(greenCircleElem)
+
+    // blue
+    let blueLabelElem = document.createElement("label")
+    blueLabelElem.classList.add("rd")
+    let blueRadioElem = document.createElement("input")
+    blueRadioElem.type = "radio"
+    blueRadioElem.name = "dp_color-picker"
+    blueRadioElem.id = "dp_blue"
+    let blueCircleElem = document.createElement("div")
+    blueCircleElem.classList.add("blue")
+    blueLabelElem.appendChild(blueRadioElem)
+    blueLabelElem.appendChild(blueCircleElem)
+    
+    // Append color options
+    colorPickerDivElem.appendChild(redLabelElem)
+    colorPickerDivElem.appendChild(orangeLabelElem)
+    colorPickerDivElem.appendChild(yellowLabelElem)
+    colorPickerDivElem.appendChild(greenLabelElem)
+    colorPickerDivElem.appendChild(blueLabelElem)
 
     // Dropdown input edit value elem
     let editValueInputElem = document.createElement("input")
     editValueInputElem.classList.add("editValueInput")
     editValueInputElem.type = "text"
-    editValueInputElem.placeholder = "kirjoita ruoka"
+    editValueInputElem.placeholder = "nimeä uudelleen..."
     editValueInputElem.value = itemValue
 
     // Dropdown button save value elem
@@ -141,19 +240,22 @@ function addNewItemToUncheckedShoppingList(item) {
     exitEditBtn.classList.add("exitEditBtn")
     exitEditBtn.textContent = "x"
 
+
+    // EVENT LISTENERS
     // On checkmark click - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     checkMarkElem.addEventListener("click", function() {
         checkMarkElem.classList.add("active")
         liElem.classList.add("active")
         window.setTimeout(() => {
-            let exactLocationOfItemInDB = ref(database, `shoppingList/unchecked/${itemID}`)
+            let exactLocationOfItemInDB = ref(database, `${shoppinglistUncheckedInDB_PATH}/${itemID}`)
             remove(exactLocationOfItemInDB)
-            push(shoppinglistCheckedInDB, itemValue)
+            push(shoppinglistCheckedInDB, {"value": itemValue, "color": itemColor})
         }, shoppingItemAnimationTime);
     })
     
-    // Dropdown triggerred - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    // Dropdown editor triggered - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     dropdownElem.addEventListener("click", function () {
+        editValueInputElem.value = itemValue
         liElem.classList.add("active")
         dropdownContentElem.classList.remove("hidden")
         console.log("editor open")
@@ -167,61 +269,87 @@ function addNewItemToUncheckedShoppingList(item) {
 
     // Save dropdown triggered - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     saveValueBtnElem.addEventListener("click", function () {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/unchecked/${itemID}`)
-        set(exactLocationOfItemInDB, editValueInputElem.value)
+        // item value
+        let newValue = editValueInputElem.value
+        if (!isInputValid(newValue)) {return}
+        let newColor = null;
+        
+        if (redRadioElem.checked) {newColor = "red"}
+        else if (orangeRadioElem.checked) {newColor = "orange"}
+        else if (yellowRadioElem.checked) {newColor = "yellow"}
+        else if (greenRadioElem.checked) {newColor = "green"}
+        else if (blueRadioElem.checked) {newColor = "blue"}
+        else {console.log("ei oo väriä mukamas")}
+
+        let exactLocationOfItemInDB = ref(database, `${shoppinglistUncheckedInDB_PATH}/${itemID}`)
+        if (newColor === null) {
+            set(exactLocationOfItemInDB, {"value": newValue, "color": itemColor})
+        } else {
+            liElem.setAttribute = ("color", itemColor)
+            set(exactLocationOfItemInDB, {"value": newValue, "color": newColor})
+        }
         dropdownContentElem.classList.add("hidden")
         liElem.classList.remove("active")
     })
 
-    dropdownContentElem.append(exitEditBtn)
-    dropdownContentElem.append(testPElem)
-    dropdownContentElem.append(editValueInputElem)
-    dropdownContentElem.append(saveValueBtnElem)
-    dropDownListElem.append(dropdownContentElem)
-    liElem.append(checkMarkElem)
-    dropdownElem.append(textElem)
-    liElem.append(dropdownElem)
-    shoppingListUncheckedElem.append(liElem)
+    // Adding the element to ul main list
+    dropdownContentElem.appendChild(exitEditBtn)
+    dropdownContentElem.appendChild(muokaaTextElem)
+    dropdownContentElem.appendChild(colorPickerDivElem)
+    dropdownContentElem.appendChild(editValueInputElem)
+    dropdownContentElem.appendChild(saveValueBtnElem)
+    dropDownListElem.appendChild(dropdownContentElem)
+    liElem.appendChild(checkMarkElem)
+    dropdownElem.appendChild(textElem)
+    liElem.appendChild(dropdownElem)
+    shoppingListUncheckedElem.appendChild(liElem)
 }
 
 
-// ========================================================================================================
-// CHECKED - - - - - - - - - - - - - - - - - - - - - 
+// =========================================================================================================================================
+// Create new CHECKED item - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 function addNewItemToCheckedShoppingList(item) {
 
     let itemID = item[0]
-    let itemValue = item[1]
+    let itemValue = item[1]["value"]
+    let itemColor = item[1]["color"]
 
+    // Main li elem
     let newLiElem = document.createElement("li")
     newLiElem.id = itemID
 
+    // Remove button elem
     let newSpanRemoveElem = document.createElement("span")
     newSpanRemoveElem.textContent = "x"
     newSpanRemoveElem.classList.add("remove_mark")
 
+    // Text value of item
     let newTextElem = document.createElement("a")
     newTextElem.textContent = itemValue
 
+    // Remove item triggered - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     newSpanRemoveElem.addEventListener("click", function() {
         newLiElem.classList.add("active")
         newSpanRemoveElem.classList.add("active")
         window.setTimeout(() => {
-            let exactLocationOfItemInDB = ref(database, `shoppingList/checked/${itemID}`)
+            let exactLocationOfItemInDB = ref(database, `${shoppinglistCheckedInDB_PATH}/${itemID}`)
             remove(exactLocationOfItemInDB)
         }, shoppingItemAnimationTime);
     })
 
+    // Uncheck item triggered - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     newTextElem.addEventListener("click", function() {
         newLiElem.classList.add("active")
         window.setTimeout(() => {
-            let exactLocationOfItemInDB = ref(database, `shoppingList/checked/${itemID}`)
+            let exactLocationOfItemInDB = ref(database, `${shoppinglistCheckedInDB_PATH}/${itemID}`)
             remove(exactLocationOfItemInDB)
-            push(shoppinglistUncheckedInDB, itemValue)
+            push(shoppinglistUncheckedInDB, {"value": itemValue, "color": itemColor})
         }, shoppingItemAnimationTime);
     })
 
-    newLiElem.append(newSpanRemoveElem)
-    newLiElem.append(newTextElem)
-    shoppingListCheckedElem.append(newLiElem)
+    // Adding the element to ul main list
+    newLiElem.appendChild(newSpanRemoveElem)
+    newLiElem.appendChild(newTextElem)
+    shoppingListCheckedElem.appendChild(newLiElem)
 }
 
